40c1688ea28fdb50401e070cf9753297ac3ba640
NavneetPrajapati31 <144175174+NavneetPrajapati31@users.noreply.github.com>
2024-01-08 19:12:35 +0530

    circular linked list

--- LEC 44-53 LINKED LIST/3_circular_linked_list.cpp
+++ LEC 44-53 LINKED LIST/3_circular_linked_list.cpp
@@ -21,17 +21,133 @@ class node
 
     //DESTRUCTOR
 
-    ~node()
-    {
-        int val = this -> data;
-        if(this->next != NULL)
-        {
+    ~node() {
+        int value = this->data;
+        if(this->next != NULL) {
             delete next;
             next = NULL;
         }
- 
-        cout<<"memory is free or node with data "<<val<<endl;
-
+        cout << " memory is free for node with data " << value << endl;
     }
 
 };
+
+void insertnode(node* &tail, int element, int d)
+{
+    //empty list
+
+    if(tail == NULL)
+    {
+        node* temp = new node(d);
+        tail = temp;
+        temp -> next = temp;
+    }
+
+    //non empty list
+
+    else
+    {
+        node* curr = tail;
+
+        while(curr->data != element)
+        {
+            curr = curr->next;
+        }
+
+        node* nodeinsert = new node(d);
+
+        nodeinsert->next = curr->next;
+        curr -> next = nodeinsert;
+
+    }
+}
+
+void printnode(node* tail)
+{
+    node* temp = tail;
+
+    if(tail == NULL)
+    {
+        cout<<"empty list"<<endl;
+        return;
+    }
+
+    do
+    {
+        cout<<tail->data<<" ";
+        tail = tail->next;
+
+    }
+
+    while(tail != temp);
+
+    cout<<endl;
+}
+
+void deletenode(node* tail, int value)
+{
+    if(tail == NULL)
+    {
+        cout<<"empty list check again";
+        return;
+    }
+
+    else
+    {
+
+        node* prev = tail;
+        node* curr = prev->next;
+
+        while(curr->data != value)
+        {
+            prev = curr;
+            curr = curr->next;
+        }
+
+        prev->next = curr->next;
+
+        //1 node linked list
+
+        if(curr == prev)
+        {
+            tail = NULL;
+        }
+
+        //>=2 node linked list
+
+        else if(tail == curr)
+        {
+            tail = prev;
+        }
+
+        curr->next = NULL;
+        delete curr;
+
+    }
+
+}
+
+
+int main()
+{
+    node* tail = NULL;
+    insertnode(tail,5,5);
+    printnode(tail); 
+
+    insertnode(tail,5,6);
+    printnode(tail); 
+
+    insertnode(tail,6,7);
+    printnode(tail);
+
+    insertnode(tail,7,8);
+    printnode(tail); 
+
+    insertnode(tail,8,9);
+    printnode(tail); 
+
+    deletenode(tail, 5);
+    printnode(tail);
+
+
+}
