e2d4a39c995cc33a542eaa121e0e022a116503d4
Navneet Prajapati <144175174+NavneetPrajapati31@users.noreply.github.com>
2024-01-23 16:35:37 +0530

    Update 10_Q_next_smaller.cpp

--- LEC 54-59 STACKS/10_Q_next_smaller.cpp
+++ LEC 54-59 STACKS/10_Q_next_smaller.cpp
@@ -6,15 +6,23 @@ using namespace std;
 vector<int> next(vector<int> &arr, int n)
 {
 stack<int> s;
+s.push(-1);
 vector<int> ans;
 
 for(int i=n-1;i>=0;i--)
 {
+curr = arr[i];
+
+while(s.top()>= curr)
+{
+s.pop();
+}
+
+ans[i] = s.top();
+s.push(curr);
 
 }
 
-
-
-
+return ans;
 
 }
