23a1124e5ba39cdab4ac4f21a1823f17badc24b4
NavneetPrajapati31 <144175174+NavneetPrajapati31@users.noreply.github.com>
2024-01-24 23:37:05 +0530

    q

--- LEC 54-59 STACKS/12_Q_celebrity_problem.cpp
+++ LEC 54-59 STACKS/12_Q_celebrity_problem.cpp
@@ -1,3 +1,172 @@
 //Celebrity Problem : https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbm11U0ZHU3N6d0pOakgyVHZvNU1JOF9jMWFZUXxBQ3Jtc0tsek9kTENNQU1tU0hrT0VESHZPWWZJQmZ6bFpzNmVrckNBc2VfOXZaZ0Z1OFRDWnlCQWlxQ2FyYUlRUzVnX0VfLW9RcENwZlZiVmpyV3pVT3hyaDFfRUlYQThYUXFxaERVZzE1MXlBSDQyUk53S19wSQ&q=https%3A%2F%2Fpractice.geeksforgeeks.org%2Fproblems%2Fthe-celebrity-problem%2F1%2F&v=9u2BJfmWNEg
 
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
 
+
+bool knows(vector<vector<int>> M, int a, int b, int n)
+{
+    if(M[a][b] == 1)
+    {
+        return true;
+    }
+
+    else
+    return false;
+
+}
+
+int celeb(vector<vector<int>> M, int n)
+{
+    //step 1 push all elements in stack
+
+    stack<int> s;
+    for(int i=0; i<n; i++)
+    {
+        s.push(i);
+    }
+
+    //step 2 get 2 elements and compare them
+
+    while(s.size() > 1)
+    {
+        int a = s.top();
+        s.pop();
+
+        int b = s.top();
+        s.pop();
+
+        if(knows(M,a,b,n) == 1)
+        {
+            s.push(b);
+        }
+
+        else
+        {
+            s.push(a);
+        }
+    }
+
+    
+    //step 3 single element is potential celeb
+
+    int ans = s.top();
+    bool rowcheck = false;
+    int zerocnt=0;
+    for(int i=0;i<n;i++)
+    {
+        if(M[ans][i] == 0)
+        {
+            zerocnt++;
+        }
+    }
+
+    if(zerocnt == n)
+    {
+        rowcheck = true;
+    }
+
+    bool colcheck = false;
+    int onecnt = 0;
+
+    for(int i=0;i<n;i++)
+    {
+        if(M[i][ans] == 1)
+        {
+            onecnt++;
+        }
+    }
+
+    if(onecnt == n-1)
+    {
+        colcheck = true;
+    }
+
+    if(rowcheck == true && colcheck == true)
+    {
+        return ans;
+    }
+
+    else
+
+    return -1;
+
+}
+
+
+
+//optimised solution
+
+
+
+int celeb_(vector<vector<int>> M, int n)
+{
+    //step 1 push all elements in stack
+
+    stack<int> s;
+    for(int i=0; i<n; i++)
+    {
+        s.push(i);
+    }
+
+    //step 2 get 2 elements and compare them
+
+    while(s.size() > 1)
+    {
+        int a = s.top();
+        s.pop();
+
+        int b = s.top();
+        s.pop();
+
+        if(knows(M,a,b,n) == 1)
+        {
+            s.push(b);
+        }
+
+        else
+        {
+            s.push(a);
+        }
+    }
+
+    
+    //step 3 single element is potential celeb
+
+    int ans = s.top();
+    
+    int zerocnt=0;
+    for(int i=0;i<n;i++)
+    {
+        if(M[ans][i] == 0)
+        {
+            zerocnt++;
+        }
+    }
+
+    if(zerocnt != n)
+    {
+        return -1;
+    }
+
+    
+    int onecnt = 0;
+
+    for(int i=0;i<n;i++)
+    {
+        if(M[i][ans] == 1)
+        {
+            onecnt++;
+        }
+    }
+
+    if(onecnt != n-1)
+    {
+        return -1;
+    }
+
+    return ans;
+
+}
--- LEC 54-59 STACKS/13_Q_max_rectangle_binary_matrix.cpp
+++ LEC 54-59 STACKS/13_Q_max_rectangle_binary_matrix.cpp
@@ -1,3 +1,108 @@
 //Max Rectangle in Binary Matrix with all 1's : https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqbjAyVXJXT0Nka29rbWdVcV9LZzFmSWtZOFBOUXxBQ3Jtc0tucVZTYTRBWVQtaUs1ZExVWjlZajQwVTBHSkRKQjVmaXYxQ1RXS0hyQzZTQ0xpR2VGOG04Y1ExWmZ6TXVMQUlBQVkzSkEyNjc2c3JNUkR0Und2X2plMVlKZzlKZVprTUdBQTlHRHhhTEUyM0dWMXR2Yw&q=https%3A%2F%2Fpractice.geeksforgeeks.org%2Fproblems%2Fmax-rectangle%2F1%2F&v=9u2BJfmWNEg
 
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
 
+#define MAX 1000
+
+
+vector<int> nextSmallerElement(vector<int> &arr, int n)
+{
+    stack<int> s;
+    s.push(-1);
+    vector<int> ans(n);
+    
+    for(int i=n-1; i>=0 ; i--) {
+        int curr = arr[i];
+        while(s.top()!= -1 && arr[s.top()] >= curr)
+        {
+            s.pop();
+        }
+        //ans is stack ka top
+        ans[i] = s.top();
+        s.push(i);
+    }
+    return ans;
+}
+
+
+vector<int> prevSmallerElement(vector<int> &arr, int n)
+{
+    stack<int> s;
+    s.push(-1);
+    vector<int> ans(n);
+    
+    for(int i=0; i<n ; i++) {
+        int curr = arr[i];
+        while(s.top()!= -1 && arr[s.top()] >= curr)
+        {
+            s.pop();
+        }
+        //ans is stack ka top
+        ans[i] = s.top();
+        s.push(i);
+    }
+    return ans;
+}
+
+
+int largestarea(vector<int> &heights, int n)
+{
+    // int n = heights.size();
+
+    vector<int> next(n);
+    next = nextSmallerElement(heights, n);
+
+    vector<int> prev(n);
+    prev = prevSmallerElement(heights, n);
+    int area = INT64_MIN;
+
+    for(int i = 0; i<n; i++)
+    {
+        int l = heights[i];
+        
+        if(next[i] == -1)
+        {
+            next[i] == n;
+        }
+
+        int b = next[i] - prev[i] - 1;
+
+        int newarea = l*b;
+
+        area = max(area, newarea);
+    }
+
+    return area;
+
+}
+
+
+int maxArea(int M[MAX][MAX], int n, int m)
+{
+    //calculate area for first row
+
+    int area = largestarea(M[0], m);
+
+    for(int i=1;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
+        {
+            if(M[i][j] != 0)
+            {
+             M[i][j] = M[i][j] + M[i-1][j];
+            }
+
+            else
+            M[i][j] = 0;
+        }
+
+        int newarea = largestarea(M[i], m);
+        area = max(area,newarea);
+    }
+
+    return area;
+
+}
