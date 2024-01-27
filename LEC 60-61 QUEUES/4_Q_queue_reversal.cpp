8e4eced0f9af12626d80ede5fa3e8cd939475126
NavneetPrajapati31 <144175174+NavneetPrajapati31@users.noreply.github.com>
2024-01-28 03:07:01 +0530

    q

---
+++ LEC 60-61 QUEUES/4_Q_queue_reversal.cpp
@@ -0,0 +1,3 @@
+//Queue Reversal : https://www.youtube.com/redirect?event=video_description&redir_token=QUFFLUhqa0VENjd0Sl9mSEVJUEw5ZnNnVEFzUW5Wb3J4UXxBQ3Jtc0trUVlIclROc1Q4V2Y0Z1FjRVdUS0RJZWx0am54SVl2a1EyT05iRlMxYllONnN1VFE0UENaYnMzaTgxaldrTmhLYjB1SFFha3haVHROcDM0Rkh5VEVhbnFnX3FQNG1rbEJwNUdHSURubkREajRYQ05aaw&q=https%3A%2F%2Fpractice.geeksforgeeks.org%2Fproblems%2Fqueue-reversal%2F1&v=_gJ3to4RyeQ
+
+
