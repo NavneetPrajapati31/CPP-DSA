9371784c4483786cf76f95a21823d728906628a0
NavneetPrajapati31 <144175174+NavneetPrajapati31@users.noreply.github.com>
2024-03-11 04:51:56 +0530

    tree

--- LEC 62-73 BINARY TREES/1_level_order_traversal.cpp
+++ LEC 62-73 BINARY TREES/1_level_order_traversal.cpp
@@ -1,3 +1,82 @@
-//
+//level order traversal: traversing the binary tree in level order i.e. top to bottom
+
+#include<iostream>
+#include<queue>
+using namespace std;
+
+class node
+{
+    public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int d)
+    {
+        this->data = d;
+        this->right = NULL;
+        this->left = NULL;
+    }
+
+};
+
+node* buildtree(node* root)
+{
+    cout<<"enter the data"<<endl;
+    int data;
+    cin>>data;
+
+    root = new node(data);
+
+    if(data == -1)
+    {
+        return NULL;
+    }
+
+    cout<<"enter data to insert at left of "<<data<<endl;
+    root->left = buildtree(root->left);
+
+    cout<<"enter data to insert at right of "<<data<<endl;
+    root->right = buildtree(root->right);
+    return root;
+}
 
 
+void levelOrderTraversal(node* root)
+{
+    queue<node*> q;
+    q.push(root);
+    q.push(NULL);
+
+    while(!q.empty())
+    {
+        node* temp = q.front();
+        q.pop();
+
+        if(temp == NULL)
+        {
+            cout<<endl;
+            if(!q.empty())
+            {
+                q.push(NULL);
+            }
+        }
+
+        else
+        {
+            cout<<temp->data<<" ";
+            if(temp->left)
+            q.push(temp->left);
+
+            if(temp->right)
+            q.push(temp->right);
+        }
+    }
+}
+
+int main()
+{
+    node* root = NULL;
+    root = buildtree(root);
+    levelOrderTraversal(root);
+}
