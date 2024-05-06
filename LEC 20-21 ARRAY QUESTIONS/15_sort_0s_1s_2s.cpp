4846a7776beeeb89e4cb50adb37436b1fb5c3222
NavneetPrajapati31 <144175174+NavneetPrajapati31@users.noreply.github.com>
2024-05-06 13:11:34 +0530

    t

--- LEC 20-21 ARRAY QUESTIONS/15_sort_0s_1s_2s.cpp
+++ LEC 20-21 ARRAY QUESTIONS/15_sort_0s_1s_2s.cpp
@@ -2,3 +2,79 @@
 //Approach 2 - count0, count1, count2 and update array
 //Approach 3 - Optimal solution - DNF algo
 
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void sortArray(vector<int>& arr, int n)
+{
+    // Write your code here
+int count0=0;
+int count1 =0;
+int count2 =0;
+
+for(int i=0;i<n;i++)
+{
+    if(arr[i] ==0)
+    {
+        count0++;
+    }
+
+    else if(arr[i] ==1)
+    {
+        count1++;
+    }
+
+    else if(arr[i] ==2)
+    {
+        count2++;
+    }
+}
+
+cout<<count0<<endl;
+cout<<count1<<endl;
+cout<<count2<<endl;
+
+int ans[n] ={0};
+int i=0;
+    while(count0>0)
+    {
+        ans[i] = 0;
+        i++;
+        count0--;
+    }
+
+    while(count1>0)
+    {
+        ans[i] = 1;
+        i++;
+        count1--;
+    }
+
+    while(count2>0)
+    {
+        ans[i] = 2;
+        i++;
+        count2--;
+    }
+
+    for(int i=0;i<n;i++)
+    {
+         cout<<ans[i]<<" ";
+     }
+
+}
+
+int main()
+{
+    vector<int> arr;
+    arr.push_back(2);
+    arr.push_back(1);
+    arr.push_back(2);
+    arr.push_back(0);
+    arr.push_back(2);
+    arr.push_back(1);
+
+    sortArray(arr,6);
+
+}
---
+++ LEC 20-21 ARRAY QUESTIONS/max subarray sum.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+#include<vector>
+#include<bits/stdc++.h>
+using namespace std;
+
+long long maxSubarraySum(vector<int> arr, int n)
+{
+    vector<vector<int>> subarrays;
+     for(int i=0;i<n;i++)
+    {
+        vector<int> temp;
+        temp.push_back(arr[i]);
+        subarrays.push_back(temp);
+
+        for(int j=i+1;j<n;j++)
+        {
+             temp.push_back(arr[j]);
+             subarrays.push_back(temp);
+        }
+    }
+
+    subarrays.push_back({});
+
+for (int i = 0; i < subarrays.size(); i++) {
+        cout << "[";
+        for (int j = 0; j < subarrays[i].size(); j++) {
+            cout << subarrays[i][j];
+            if (j < subarrays[i].size() - 1) {
+                cout << ", ";
+            }
+        }
+        cout << "]";
+        if (i < subarrays.size() - 1) {
+            cout << ", ";
+        }
+}
+
+vector<int> sums;
+int sum=0;
+for (int i = 0; i < subarrays.size(); i++) {
+    sum=0;
+        for (int j = 0; j < subarrays[i].size(); j++) {
+            sum+=subarrays[i][j];
+        }
+        sums.push_back(sum);
+}
+
+cout<<endl;
+
+for(int i=0;i<sums.size();i++)
+{
+    cout<<sums[i]<<" ";
+}
+
+long long  maxi =INT32_MIN;
+for(int i=0;i<sums.size();i++)
+{
+    if(sums[i]>maxi)
+    {
+        maxi = sums[i];
+    }
+}
+return maxi;
+}
+
+
+int main()
+{
+    vector<int> arr;
+    arr.push_back(-7);
+    arr.push_back(-8);
+    arr.push_back(-16);
+    arr.push_back(-4);
+    arr.push_back(-8);
+    arr.push_back(-5);
+        arr.push_back(-7);
+        arr.push_back(-11);
+        arr.push_back(-10);
+                arr.push_back(-12);
+                        arr.push_back(-4);
+                                arr.push_back(-6);
+                                        arr.push_back(-4);
+                                                arr.push_back(-16);
+                                                        arr.push_back(-10);
+
+cout<<maxSubarraySum(arr, 15);
+    
+    }
