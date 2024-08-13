fc55b3942b5b7c998cbdce44a17b77ebc829330e
Navneet Prajapati <navneetprajapati46@gmail.com>
2024-08-14 01:27:41 +0530

    dsa

--- LEC 69-73 BINARY SEARCH TREES/7_Q_normal_to_balanced_BST.cpp
+++ LEC 69-73 BINARY SEARCH TREES/7_Q_normal_to_balanced_BST.cpp
@@ -1,2 +1,35 @@
 //https://www.naukri.com/code360/problems/normal-bst-to-balanced-bst_920472?leftPanelTab=0
 
+void inorder(TreeNode<int>* root, vector<int> &in)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+
+    inorder(root->left,in);
+    in.push_back(root->data);
+    inorder(root->right,in);
+}
+
+TreeNode<int>* solve(vector<int> in, int s, int e)
+{
+    if(s>e)
+     return NULL;
+
+    int mid = s+(e-s)/2;
+    TreeNode<int>* root = new TreeNode<int>(in[mid]);
+
+    root->right = solve(in,mid+1,e);
+    root->left = solve(in,s,mid-1);
+    return root;
+}
+
+TreeNode<int>* balancedBst(TreeNode<int>* root)
+{
+    // Write your code here.
+    vector<int> in;
+    inorder(root,in);
+
+    return solve(in,0,in.size()-1);
+}
--- LEC 69-73 BINARY SEARCH TREES/8_Q_preorder_trraversal_of_BST.cpp
+++ LEC 69-73 BINARY SEARCH TREES/8_Q_preorder_trraversal_of_BST.cpp
@@ -1,2 +1,26 @@
 //https://www.naukri.com/code360/problems/preorder-traversal-to-bst_893111?leftPanelTab=0
 
+BinaryTreeNode<int>* solve(vector<int> &pre, int &i, int min, int max)
+{
+    if(i>=pre.size())
+     return NULL;
+
+    if(pre[i] > max || pre[i]<min)
+    {
+        return NULL;
+    }
+
+    BinaryTreeNode<int>* node = new BinaryTreeNode<int>(pre[i++]);
+    node->left = solve(pre,i,min,node->data);
+    node->right = solve(pre,i,node->data,max);
+
+    return node;
+}
+
+BinaryTreeNode<int>* preorderToBST(vector<int> &preorder) {
+    // Write your code here.
+    int i=0;
+    int mini = INT_MIN;
+    int maxi = INT_MAX;
+    return solve(preorder,i,mini,maxi);
+}
